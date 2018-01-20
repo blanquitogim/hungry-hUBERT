e690f33728f153940a41daab5f80917ebacdc1dc
blanquitogim <34944750+blanquitogim@users.noreply.github.com>
2018-01-20 12:48:26 +0100

    Add files via upload

--- article.h
+++ article.h
@@ -1,7 +1,20 @@
+#include <signal.h>
+#ifndef ARTICLE_H
+#define ARTICLE_H
+#define NUMERO_MAX_RESTO 3
+#define MUTEX 1
+#define NUMERO_MAX_COMMANDES 10
+
 //	Structure d'un article
-#define CLEF 3
+#define CLEF 100
+#define CLEF_SEM 110
+
+#define CLEF_HUBERT_RESTO 120
+#define CLEF_HUBERT_RESTO_SEM 130
 #define NUMERO_MAX_ARTICLES 5
 #define QUANTITE_MAX_PLATS 10
+
+static int compteurResto;
 
 typedef struct ARTICLE{
 	int type;
@@ -9,8 +22,8 @@ typedef struct ARTICLE{
   double prix;
 } ARTICLE;
 
+typedef struct COMMANDE{
+	ARTICLE commande [NUMERO_MAX_ARTICLES*NUMERO_MAX_RESTO];
+}COMMANDE;
 
-
-/*typedef struct LISTE_ARTICLES{
-	ARTICLE* tab[NUMERO_MAX_ARTICLES];
-} LISTE_ARTICLES;*/
+#endif
