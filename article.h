c37d124bda107e21e5d7dc376cc8c2a9e4ea7097
hannah888 <35363577+hannah888@users.noreply.github.com>
2018-01-20 15:58:25 +0100

    Update article.h

--- article.h
+++ article.h
@@ -1,6 +1,7 @@
 #include <signal.h>
 #ifndef ARTICLE_H
 #define ARTICLE_H
+
 #define NUMERO_MAX_RESTO 3
 #define MUTEX 1
 #define NUMERO_MAX_COMMANDES 10
@@ -19,11 +20,13 @@ static int compteurResto;
 typedef struct ARTICLE{
 	int type;
 	int quantite;
-  double prix;
+  	double prix;
 } ARTICLE;
 
 typedef struct COMMANDE{
-	ARTICLE commande [NUMERO_MAX_ARTICLES*NUMERO_MAX_RESTO];
+	int restau ;
+  	int pid_client ;
+	ARTICLE commande [NUMERO_MAX_ARTICLES];
 }COMMANDE;
 
 #endif
